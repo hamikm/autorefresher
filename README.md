8b234f9b1ddd8fb01f056a6d58a117f6f6d281a6
Hamik Mukelyan <hamik@uber.com>
2017-04-05 16:31:45 -0700

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # autorefresher
-Periodically checks a webpage for updates and emails them, if any, to you
+Periodically checks a webpage for updates and emails them, if any, to you. Replace the `TODO`s in the script with your info, enable "insecure apps" in Gmail (ideally not on your real account), and run the script in the background.
---
+++ auto_refresher.py
@@ -0,0 +1,52 @@
+import smtplib
+import urllib2
+import time
+from HTMLParser import HTMLParser
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from dateutil.parser import parse
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+old_part_of_interest = ''
+
+def sendEmail(recipients, new_content_html):
+	fromaddr = TODO # string
+	msg = MIMEMultipart()
+	msg['From'] = fromaddr
+	msg['Subject'] = TODO # string
+	 
+	msg.attach(MIMEText(new_content_html, 'html'))
+	 
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.starttls()
+	password = TODO # string
+	server.login(fromaddr, password)
+	text = msg.as_string()
+	server.sendmail(fromaddr, recipients, text)
+	server.quit()
+
+def getHTMLFromPage(url):
+	import urllib2
+	response = urllib2.urlopen(url)
+	return response.read()
+
+def getPartOfInterestFromRawHTML(raw_html):
+	parsedDoc = BeautifulSoup(raw_html, 'html.parser')
+	lookForThisTag = TODO # string
+	strStrDictOfAttrbs = TODO # e.g.  { 'class' : 'glossaryProduct' }
+	return parsedDoc.find(lookForThisTag, strStrDictOfAttrbs).prettify()
+
+if __name__ == '__main__':
+	while True:
+		theUrl = TODO # string
+		raw_html = getHTMLFromPage(theUrl)
+		part_of_interest = getPartOfInterestFromRawHTML(raw_html)
+		if part_of_interest != old_part_of_interest:
+			html_email = '<html><head></head><body>' + part_of_interest + '</body></html>'
+			lstOfRecipients = TODO # e.g. ['a@.com', 'c@d.com'] 
+			sendEmail(lstOfRecipients, html_email)
+			old_part_of_interest = part_of_interest
+		sleepTimeMins = TODO # int
+		time.sleep(60 * sleepTimeMins)
+
